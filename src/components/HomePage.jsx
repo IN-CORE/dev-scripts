0ba8076af4308b6aad4d426cc9c82f018c0aa1ab
Omar Elabd <oelabd@illinois.edu>
2017-02-09 11:05:52 -0600

    initial commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router";
+
+type Props = {
+	name: string
+}
+
+const HomePage = (props: Props) => {
+	return (
+		<div>
+			<h1>IN-CORE v3 Template</h1>
+
+			<h2>Get Started</h2>
+			<ol>
+			</ol>
+		</div>
+	);
+};
+
+export default HomePage;

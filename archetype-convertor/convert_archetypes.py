14c6e7e733024d8cfb8f74f237a9c2898a2e3b3e
vnarah2 <vnarah2@illinois.edu>
2020-11-02 10:29:56 -0600

    INCORE1-850: Fixed the case when no buildings fall under some damage states

--- archetype-convertor/convert_archetypes.py
+++ archetype-convertor/convert_archetypes.py
@@ -18,11 +18,14 @@ def create_mapped_result(inventory_path: str, dmg_result_path: str, archetype_ma
     dmg_result = pd.read_csv(dmg_result_path)
     arch_mapping = pd.read_csv(archetype_mapping_path)
 
+    # TODO: Do not hardcode this and fetch them from the damage result columns, if it's possible
+    dmg_states = ['insignific', 'moderate', 'heavy', 'complete']
+
     unique_categories = arch_mapping.groupby(by=['cluster', 'category'], sort=False).count().reset_index()
 
     guids = dmg_result[['guid']]
-    max_val = dmg_result[['insignific', 'moderate', 'heavy', 'complete']].max(axis=1)
-    max_key = dmg_result[['insignific', 'moderate', 'heavy', 'complete']].idxmax(axis=1)
+    max_val = dmg_result[dmg_states].max(axis=1)
+    max_key = dmg_result[dmg_states].idxmax(axis=1)
     dmg_concat = pd.concat([guids, max_val, max_key], axis=1)
     dmg_concat.rename(columns={0: 'max_prob', 1: 'max_state'}, inplace=True)
     dmg_merged = pd.merge(buildings, dmg_concat, on='guid')[['guid', 'geometry', 'archetype', 'max_prob', 'max_state']]
@@ -39,12 +42,15 @@ def create_mapped_result(inventory_path: str, dmg_result_path: str, archetype_ma
     table = pd.DataFrame()
     table[['category', 'cluster']] = unique_categories[['category', 'cluster']]
     result_by_cluster = pd.merge(table, pivot, how='left', on=['cluster', 'category'])
+
+    # Add missing max damage states. Handles case when no buildings fall under some damage states.
+    result_by_cluster = result_by_cluster.reindex(result_by_cluster.columns.union(
+        dmg_states, sort=False), axis=1, fill_value=0)
+
     result_by_category = result_by_cluster.groupby(by=['category'], sort=False).sum(min_count=1).reset_index()
 
-    result_by_cluster[['insignific', 'moderate', 'heavy', 'complete']] = result_by_cluster[
-        ['insignific', 'moderate', 'heavy', 'complete']].fillna(-1).astype(int)
-    result_by_category[['insignific', 'moderate', 'heavy', 'complete']] = result_by_category[
-        ['insignific', 'moderate', 'heavy', 'complete']].fillna(-1).astype(int)
+    result_by_cluster[dmg_states] = result_by_cluster[dmg_states].fillna(-1).astype(int)
+    result_by_category[dmg_states] = result_by_category[dmg_states].fillna(-1).astype(int)
 
     cluster_records = result_by_cluster.to_json(orient="records")
     category_records = result_by_category.to_json(orient="records")

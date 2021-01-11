cc7cd60b9819c355851250a1b59d04d5ee2d03c5
Yong Wook Kim <ywkim@illinois.edu>
2021-01-11 12:05:35 -0600

    changed key file location

--- utils/webdav_utils/match_dataset_format_and_data.py
+++ utils/webdav_utils/match_dataset_format_and_data.py
@@ -19,9 +19,9 @@ import os
 from osgeo import gdal
 
 MONGO_DB = "datadb"
-MONGO_USER = "ubuntu"
+MONGO_USER = ""
 MONGO_PASS = "PASSWORD"
-MONGO_KEYFILE = "C:\\Users\\ywkim\\.ssh\\nist.pem"
+MONGO_KEYFILE = "path_to_your_keyfile"
 MONGO_BIND_HOST = "127.0.0.1"
 MONGO_BIND_PORT = 27017
 

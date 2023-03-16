55f9f77ecab75e02fe999e7698bfdac9c52269a2
Chen Wang <cwang138@illinois.edu>
2023-03-16 12:53:08 -0500

    uncomment

--- incore-services/sync_users_roles_steps_playbook.py
+++ incore-services/sync_users_roles_steps_playbook.py
@@ -163,18 +163,18 @@ if __name__ == "__main__":
             "group_name": "incore_slc_user",
             "group_id": os.getenv("SLC_GROUP_ID")  # get this information from keycloak
         },
-        # {
-        #     "testbed": "galveston",
-        #     "url": server_base_url + "/maestro/galveston",
-        #     "group_name": "incore_galveston_user",
-        #     "group_id":  os.getenv("JOPLIN_GROUP_ID")
-        # },
-        # {
-        #     "testbed": "joplin",
-        #     "url": server_base_url + "/maestro/joplin",
-        #     "group_name": "incore_joplin_user",
-        #     "group_id": os.getenv("GALVESTON_GROUP_ID")  # get this information from keycloak
-        # },
+        {
+            "testbed": "galveston",
+            "url": server_base_url + "/maestro/galveston",
+            "group_name": "incore_galveston_user",
+            "group_id":  os.getenv("JOPLIN_GROUP_ID")
+        },
+        {
+            "testbed": "joplin",
+            "url": server_base_url + "/maestro/joplin",
+            "group_name": "incore_joplin_user",
+            "group_id": os.getenv("GALVESTON_GROUP_ID")  # get this information from keycloak
+        },
     ]
 
     for item in config:

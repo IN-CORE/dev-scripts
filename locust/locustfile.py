579f061d50bafe7500cf6fa97fba60d1cf8f2d1c
Ya-Lan Yang <ylyang@illinois.edu>
2023-11-09 18:27:31 -0600

    Update locustfile.py

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,90 +1,232 @@
 import json
-from urllib.parse import urljoin
-
+import logging
+import mimetypes
+from http.client import HTTPConnection
 from locust import HttpUser, task, between
-
+from codecs import encode
 
 class MyUser(HttpUser):
     wait_time = between(1, 5)  # Add the desired wait time between tasks
-    headers = {"content-type" : "application/x-www-form-urlencoded", "Authorization": "bearer {Your Authenication}"}
+
+    headers = {
+        "Authorization": "bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJfa1pWbFM4OU1SVWNZaURYUjByaGVVSUh6RzdZWXZ4eXcxZ1l3WjJZajVjIn0.eyJleHAiOjE2OTk1ODA3MTcsImlhdCI6MTY5OTU2NjMxNywianRpIjoiZThjMDNhZTMtYTI4YS00M2NiLTgyODUtZDI3YzU4MDQ0ODExIiwiaXNzIjoiaHR0cHM6Ly9pbmNvcmUubmNzYS5pbGxpbm9pcy5lZHUvcmVhbG1zL0luLWNvcmUiLCJhdWQiOlsiSU5DT1JFIiwiYWNjb3VudCJdLCJzdWIiOiIyNTNlNWU3MC0wYjU0LTQ5MDEtODc3NS1jOWZmN2I1MzViOGEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJyZWFjdC1hdXRoIiwic2Vzc2lvbl9zdGF0ZSI6Ijc0YWQ2OWEzLTU1NGUtNDE2ZS05M2EwLTY1ZmNkZjg5OWM3OSIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDozMDAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJkZWZhdWx0LXJvbGVzLWluLWNvcmUiLCJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIiwiaW5jb3JlX3VzZXJfcm9sZSJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6InByb2ZpbGUgZW1haWwiLCJzaWQiOiI3NGFkNjlhMy01NTRlLTQxNmUtOTNhMC02NWZjZGY4OTljNzkiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwidWlkX251bWJlciI6NjcxMjYsIm5hbWUiOiJZYS1MYW4gWWFuZyIsImdyb3VwcyI6WyJpbmNvcmVfam9wbGluX3VzZXIiLCJpbmNvcmVfbmNzYSIsImluY29yZV9nYWx2ZXN0b25fdXNlciIsImluY29yZV9hZG1pbiIsImluY29yZV9zbGNfdXNlciIsImluY29yZV91c2VyIl0sInByZWZlcnJlZF91c2VybmFtZSI6InlseWFuZyIsImdpdmVuX25hbWUiOiJZYS1MYW4iLCJmYW1pbHlfbmFtZSI6IllhbmciLCJlbWFpbCI6InlseWFuZ0BpbGxpbm9pcy5lZHUifQ.JStC5W8bFds4M1_KSMBphfP_BCP-BleUYddr3dwBYdSpI0OZ4lfIhEQ3gNBmDueQMB0Ldqo_8AsPcTo4KOYv_f8rUuHzOZ2Wz6IK1tNl2iykozQzx1xPMIexYhlpLs5wcE-dcHKSNv70e6pCH6o9yzlib1b27bo9zlLFxIhadMPbcoglZRJSxtLqL0qXatdGiq4s0qRztt0S4XlovC3drsPXi5OF_na0JGSWQsbN9CC-Y5-YaBWtty8-ZpkdGsoKaNH85FNKtvZr7P7Uwz8lcPNJzriWmueC8rXXwfGXZtd54qtGHt1iLm2PSMVXktkNHMZ6nG_bWxCoksomHQNwNg"
+    }
+
+    ##### Debugging #####
+    # HTTPConnection.debuglevel = 1
+    # logging.basicConfig()
+    # logging.getLogger().setLevel(logging.DEBUG)
+    # requests_log = logging.getLogger("requests.packages.urllib3")
+    # requests_log.setLevel(logging.DEBUG)
+    # requests_log.propagate = True
 
     @task
-    def hazard(self):
-        earthquake_id = "5b902cb273c3371e1236b36b";
+    def geoserver(self):
+        self.client.get(
+            "/geoserver/incore/wms?service=WMS&version=1.1.0&request=GetMap&layers=incore%3A5f4d02e352147a614c71960b&bbox=-79.212607077%2C34.562496808%2C-78.973249171%2C34.705963648&width=768&height=460&srs=EPSG%3A4326&styles=&format=image/png")
 
-        form_data = {
-           "name":"yytest",
-           "description":"yytest",
-           "eqType":"model",
-           "attenuations":{
-              "ChiouYoungs2014":"1.0"
-           },
-           "eqParameters":{
-              "srcLatitude":40.177,
-              "srcLongitude":-111.426,
-              "magnitude":"5.5",
-              "depth":"11.2",
-              "region":"",
-              "faultTypeMap":{
-                 "ChiouYoungs2014":"Normal"
-              }
-           },
-           "visualizationParameters":{
-              "demandType":"PGA",
-              "demandUnits":"g",
-              "minX":-112.312,
-              "minY":40.277,
-              "maxX":-111.462,
-              "maxY":41,
-
-              "numPoints":"1025",
-              "amplifyHazard":"true"
-           }
+    @task
+    def hazard_earthquake(self):
+        id = "5b902cb273c3371e1236b36b";
+        url = "hazard/api/earthquakes/"
+        earthquake_model_json = {
+            "name": "Memphis EQ Model (modified)",
+            "description": "Memphis model based hazard",
+            "eqType": "model",
+            "attenuations": {
+                "AtkinsonBoore1995": "1.0"
+            },
+            "eqParameters": {
+                "srcLatitude": "35.927",
+                "srcLongitude": "-89.919",
+                "magnitude": "8.1",
+                "depth": "7.0"
+            },
+            "visualizationParameters": {
+                "demandType": "PGA",
+                "demandUnits": "g",
+                "minX": "-90.3099",
+                "minY": "34.9942",
+                "maxX": "-89.6231",
+                "maxY": "35.4129",
+                "numPoints": "1025",
+                "amplifyHazard": "true"
+            }
         }
+        points = [
+            {
+                "demands": ["0.2 SA"],
+                "units": ["g"],
+                "loc": "35.07899, -90.0178"
+            },
+            {
+                "demands": ["0.2 SA", "PGA", "0.8 SA"],
+                "units": ["g", "g", "g"],
+                "loc": "35.027, -90.077"
+            }
+        ]
 
-        context = """
-                {
-           "name":"yytest",
-           "description":"yytest",
-           "eqType":"model",
-           "attenuations":{
-              "ChiouYoungs2014":"1.0"
-           },
-           "eqParameters":{
-              "srcLatitude":40.177,
-              "srcLongitude":-111.426,
-              "magnitude":"5.5",
-              "depth":"11.2",
-              "region":"",
-              "faultTypeMap":{
-                 "ChiouYoungs2014":"Normal"
-              }
-           },
-           "visualizationParameters":{
-              "demandType":"PGA",
-              "demandUnits":"g",
-              "minX":-112.312,
-              "minY":40.277,
-              "maxX":-111.462,
-              "maxY":41,
-             
-              "numPoints":"1025",
-              "amplifyHazard":"true"
-           }
+        # GET /earthquakes/{id}
+        self.client.get(f"{url}{id}", headers=self.headers)
+
+        # POST /earthquake
+        self.post(url, self.headers, earthquake_model_json, "earthquake")
+
+        # POST /earthquakes/{id}/values
+        #self.post_values(url, id, self.headers, points, "earthquake")
+
+    @task
+    def hazard_tornado(self):
+        id = "60a44ae8605f0462bd4263ac";
+        url = "hazard/api/tornadoes/"
+        tornado_json_model = {
+            "name": "Centerville Model Tornado (modified)",
+              "description": "Centerville mean width tornado hazard",
+              "tornadoType": "model",
+              "tornadoModel" : "MeanWidthTornado",
+                  "tornadoParameters" : {
+                  "efRating" : "EF4",
+                  "startLatitude" : "35.215",
+                  "startLongitude" : "-97.524",
+                  "randomSeed" : "3457",
+                  "endLatitude" : [35.253],
+                  "endLongitude" : [-97.432],
+                  "windSpeedMethod" : "1",
+                  "numSimulations" : "1"
+                }
         }
-        """
+        tornado_json_dataset = {
+            "name": "Joplin Dataset Tornado - workshop",
+            "description": "Joplin tornado hazard with shapefile",
+            "tornadoType": "dataset"
+        }
+        shapefile_location = '/Users/ylyang/incore-workshop/inspire-workshop-2023-11-15/session2/data/hazard/tornado/joplin_path_wgs84.shp'
+        points = [
+            {
+                "demands": ["wind"],
+                "units": ["mph"],
+                "loc": "35.215, -97.521"
+            },
+            {
+                "demands": ["wind"],
+                "units": ["mph"],
+                "loc": "35.215, -97.519"
+            }
+        ]
 
 
-        self.client.get(f"hazard/api/earthquakes/{earthquake_id}", headers=self.headers)
-        #self.client.post("hazard/api/earthquakes/", json=json.dumps(form_data), headers=self.headers)
+        # GET /tornadoes/{id}
+        self.client.get(f"{url}{id}", headers=self.headers)
+
+        # POST /tornadoes
+        self.post(url, self.headers, tornado_json_model, "tornado")
+
+        # POST /tornadoes
+        #self.post_with_file(url, self.headers, tornado_json_dataset, "tornado", shapefile_location)
 
 
+
+
+
+    def post(self, url, headers, form_data, hazard):
+        dataList = []
+        boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
+        dataList.append(encode('--' + boundary))
+        dataList.append(encode(f'Content-Disposition: form-data; name={hazard};'))
+
+        dataList.append(encode('Content-Type: {}'.format('text/plain')))
+        dataList.append(encode(''))
+
+        dataList.append(encode(json.dumps(form_data)))
+        dataList.append(encode('--' + boundary + '--'))
+        dataList.append(encode(''))
+        body = b'\r\n'.join(dataList)
+        payload = body
+        headers = headers | {'Content-type': 'multipart/form-data; boundary={}'.format(boundary)}
+
+        res = self.client.post(f"{url}", data=payload, headers=headers)
+        if res.status_code == 200:
+            id = res.json()['id']
+            self.client.delete(f"{url}{id}", headers=headers)
+
+    def post_with_file(self, url, headers, form_data, hazard, shapefile_location):
+        dataList = []
+        boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
+        dataList.append(encode('--' + boundary))
+        dataList.append(encode(f'Content-Disposition: form-data; name={hazard};'))
+
+        dataList.append(encode('Content-Type: {}'.format('text/plain')))
+        dataList.append(encode(''))
+
+        dataList.append(encode(json.dumps(form_data)))
+        dataList.append(encode('--' + boundary))
+        dataList.append(
+            encode('Content-Disposition: form-data; name=file; filename={0}'.format('joplin_path_wgs84.shp')))
+
+        fileType = mimetypes.guess_type(
+            shapefile_location)[
+                       0] or 'application/octet-stream'
+        dataList.append(encode('Content-Type: {}'.format(fileType)))
+        dataList.append(encode(''))
+
+        with open(shapefile_location, 'rb') as f:
+            dataList.append(f.read())
+        dataList.append(encode('--' + boundary + '--'))
+        dataList.append(encode(''))
+        body = b'\r\n'.join(dataList)
+        payload = body
+        headers = headers | {'Content-type': 'multipart/form-data; boundary={}'.format(boundary)}
+
+        res = self.client.post(f"{url}", data=payload, headers=headers)
+        if res.status_code == 200:
+            id = res.json()['id']
+            self.client.delete(f"{url}{id}", headers=headers)
+
+    def post_values(self, url, id, headers, form_data, hazard):
+        dataList = []
+        boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
+        dataList.append(encode('--' + boundary))
+        dataList.append(encode('Content-Disposition: form-data; name=points;'))
+
+        dataList.append(encode('Content-Type: {}'.format('text/plain')))
+        dataList.append(encode(''))
+
+        dataList.append(encode(form_data))
+        dataList.append(encode('--' + boundary + '--'))
+        dataList.append(encode(''))
+        body = b'\r\n'.join(dataList)
+        payload = body
+        headers = headers | {'Content-type': 'multipart/form-data; boundary={}'.format(boundary)}
+
+        self.client.post(f"{url}{id}/values", data=payload, headers=headers)
 
     @task
     def dataset(self):
-        self.client.get("data/api/datasets/5d07cbe9b9219c065b819103", headers=self.headers)
+        # Get tornado dataset (60a44ae8605f0462bd4263ac)
+        self.client.get("data/api/datasets/60a44ae77da24b7b5ba0e86f", headers=self.headers)
+
+        # Get building dataset (5dbc8478b9219c06dd242c0d)
         self.client.get("data/api/datasets/5dbc8478b9219c06dd242c0d", headers=self.headers)
+
+        # Get socio_demographic_data = "60fb4241544e944c3cedb507"
         self.client.get("data/api/datasets/60fb4241544e944c3cedb507", headers=self.headers)
+
+        # Get financial_resources = "60fb411060b3f4124301f95a"
         self.client.get("data/api/datasets/60fb411060b3f4124301f95a", headers=self.headers)
+
+        # Get delay_factors = "60fb433cd3c92a78c89d21cc"
         self.client.get("data/api/datasets/60fb433cd3c92a78c89d21cc", headers=self.headers)
+
+        # Get 64ee0bcd553ecf0768e21e55
         self.client.get("data/api/datasets/64ee0bcd553ecf0768e21e55", headers=self.headers)
+
+    @task
+    def dfr3(self):
+        # GET / fragilities / {id}
+        self.client.get("dfr3/api/fragilities/5b47b2d7337d4a36187c61c9", headers=self.headers)
+
+        # GET / mappings {id}
+        self.client.get("dfr3/api/mappings/5b47b2d9337d4a36187c7564", headers=self.headers)
+
+        # GET / repairs / {id}
+        self.client.get("dfr3/api/repairs/6513058862ba036575da5fca", headers=self.headers)

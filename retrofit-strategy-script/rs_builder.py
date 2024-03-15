5b9d7834bb15e389f271ae97a5e865963b27aaa3
Chen Wang <cwang138@illinois.edu>
2024-03-15 11:39:24 -0500

    initial clean up

--- retrofit-strategy-script/rs_builder.py
+++ retrofit-strategy-script/rs_builder.py
@@ -1,8 +1,14 @@
+import os
+
 import duckdb
 import geopandas as gpd
 import pandas as pd
 import argparse
 import json
+import requests
+from pyincore import IncoreClient, DataService, SpaceService
+from scipy import signal
+
 import retrofit_cost_slc as rc_slc
 import retrofit_cost_galveston as rc_galveston
 
@@ -28,18 +34,21 @@ JOPLIN_CONFIG = {
     "zone_col": "name"
 }
 
+
 ### get duckdb connection with spatial extension
 def get_connection(db_file):
     conn = duckdb.connect(db_file, read_only=False)
     # checking spatial extention and install
-    spatial_extension_query = conn.execute("SELECT * FROM duckdb_extensions() WHERE installed IS TRUE AND extension_name = 'spatial';").fetchone()
+    spatial_extension_query = conn.execute(
+        "SELECT * FROM duckdb_extensions() WHERE installed IS TRUE AND extension_name = 'spatial';").fetchone()
     if spatial_extension_query is None:
         print("Installing DuckDB spatial extension...")
         conn.execute("INSTALL spatial;")
-    
+
     # loading spatial extension
     conn.execute("LOAD spatial;")
     return conn
+
 
 ### Get all the buildings in a specific boundary with a specific structure type
 def get_buildings(conn, config, struct_typ, bnd_name):
@@ -48,9 +57,9 @@ def get_buildings(conn, config, struct_typ, bnd_name):
         add_col_str += ", " + col
 
     sql_str = f"SELECT guid, ST_AsText(geom) as geom{add_col_str} FROM {config['bldg_table_name']} " + \
-            f"WHERE {config['structure_type_col']}='{struct_typ.upper()}' " + \
-            f"AND {config['zone_col']}='{bnd_name.upper()}';"
-    
+              f"WHERE {config['structure_type_col']}='{struct_typ.upper()}' " + \
+              f"AND {config['zone_col']}='{bnd_name.upper()}';"
+
     rel = conn.sql(sql_str)
     print("# of buildings selected:", rel.shape[0])
 
@@ -61,7 +70,7 @@ def get_buildings(conn, config, struct_typ, bnd_name):
 def create_retrofit_strategy_by_rule(idx, rel, percent, retrofit_key, retrofit_val):
     df = rel.to_df()
     # selecting randm buildings with percent
-    df_sample = df.sample(frac=percent/100.0)
+    df_sample = df.sample(frac=percent / 100.0)
 
     # add retrofit key and value to the dataframe
     idx_list = [idx] * df_sample.shape[0]
@@ -74,22 +83,27 @@ def create_retrofit_strategy_by_rule(idx, rel, percent, retrofit_key, retrofit_v
     print("# of buildings sampled:", df_sample.shape[0], "/", df.shape[0])
     return df_sample
 
+
 def merge_create_retrofit_strategy(df_list, result_name):
     df = pd.concat(df_list)
-    print(df.shape)
-    df[['guid','retrofit_key','retrofit_value']].to_csv(result_name + ".csv", index=False)
-    return df
+    rs_csv_name = result_name.replace(" ", "_") + ".csv"
+    df[['guid', 'retrofit_key', 'retrofit_value']].to_csv(rs_csv_name, index=False)
+    return df, rs_csv_name
+
 
 def create_geo_retrofit_strategy(df, result_name):
-    gdf = gpd.GeoDataFrame(df,geometry= gpd.GeoSeries.from_wkt(df['geom']),crs="EPSG:4326")
+    gdf = gpd.GeoDataFrame(df, geometry=gpd.GeoSeries.from_wkt(df['geom']), crs="EPSG:4326")
     gdf.drop(columns=['geom'], inplace=True)
-    gdf.to_file(result_name + ".shp")
-    return gdf
+    shapefile_name = result_name.replace(" ", "_") + "_details.shp"
+    gdf.to_file(shapefile_name)
+    return gdf, shapefile_name
+
 
 ### Parse the retrofit strategy rules
 def parse_rules(rules_str):
     rules = json.loads(rules_str)
     return rules
+
 
 ### Parse the retrofits
 def parse_retrofits(retrofits_str):
@@ -97,80 +111,79 @@ def parse_retrofits(retrofits_str):
     return retrofits
 
 
+def handler(signum, frame):
+    print("Forever is over!")
+    raise Exception("end of time")
 
 
-if __name__ == "__main__":
+def store_results(dataservice, spaceservice, source_id, title, local_file, data_type, output_format, spaces):
+    output_properties = {"dataType": data_type, "title": title + "- test", "format": output_format,
+                         "sourceDataset": source_id}
 
-    # parser = argparse.ArgumentParser(description='Generating Retrofit Strategy Dataset and Computing Retrofit Cost')
-    # parser.add_argument('--strategy_result_name', dest='strategy_result_name', help='Strategy Result name')
-    # parser.add_argument('--cost_result_name', dest='cost_result_name', help='Cost result name')
-    # parser.add_argument('--rules', dest='rules', type=json.loads, help='Retrofit strategy rules')
-    # parser.add_argument('--retrofits', dest='retrofits', type=json.loads, help='Retrofits')
+    # register the handler
+    signal.signal(signal.SIGALRM, handler)
 
-    # args = parser.parse_args()
+    if output_format == "shapefile":
+        files = []
+        file_name = os.path.splitext(local_file)[0]
+        print("Looking for shapefiles matching on: " + file_name)
+        for shp_file in os.listdir("."):
+            if shp_file.startswith(file_name):
+                files.append(os.path.join(".", shp_file))
+    else:
+        files = [str(os.path.join(".", local_file))]
 
-    # rules = parse_rules(args.rules_str)
-    # retrofits = parse_rules(args.retrofits_str) 
+    # print("does file exist?")
+    # print(path.exists(os.path.join(".", local_file)))
 
-    ### testing with 1 rule for Galveston
-    # rules = {
-    #     "testbed":"galveston",
-    #     "rules": 1,
-    #     "zones": ["0.2P"],
-    #     "strtypes": ["1"],
-    #     "pcts": [5]
-    # }
-    # retrofits = {
-    #     "ret_keys":["elevation"],
-    #     "ret_vals":[5]        
-    # }
+    dataset_id = "temp-id"
+    # Use this for testing locally
+    save_to_service = True
+    if save_to_service:
+        response = dataservice.create_dataset(output_properties)
+        dataset_id = response['id']
+        print("created dataset" + dataset_id)
+        # Add file to spaces
+        for space in spaces:
+            spaceservice.add_to_space_by_name(space, dataset_id)
 
-    ### testing with 3 rule for Galveston
-    rules = {
-        "testbed":"galveston",
-        "rules": 3,
-        "zones": ["1P", "1P", "0.2P"],
-        "strtypes": ["1", "2", "1"],
-        "pcts": [1,1,1]
+    print("saving output dataset id")
+    output_dataset_id = open(title + "-output_id.txt", "w")
+    output_dataset_id.write(dataset_id)
+    output_dataset_id.close()
+
+    if save_to_service:
+        try:
+            # time out after 1 min so tool doesn't get stuck waiting for big files that return responses slowly
+            signal.alarm(60)
+
+            print("adding files to dataset")
+            dataservice.add_files_to_dataset(dataset_id, files)
+        except Exception as exc:
+            print(exc)
+
+    return dataset_id
+
+
+def post_retrofit_summary(service_url, rs_dataset_id, rules_q, retrofits_q, rs_details_dict, rs_details_layer_id):
+    rs_details_summary = {
+        **rs_details_dict,
+        "rules": rules_q,
+        "retrofits": retrofits_q,
+        "rsDetailsLayerId": rs_details_layer_id,
     }
-    retrofits = {
-        "ret_keys":["elevation","elevation","elevation"],
-        "ret_vals":[5, 10, 5]        
-    }
+    response = requests.post(f"{service_url}/maestro/datasets/{rs_dataset_id}/rsdetails", json=rs_details_summary)
+    if response.status_code != 200:
+        print("Failed to post retrofit strategy summary to the maestro service.")
+        print(response.text)
+        return None
 
-    #### testing with 3 rules for SLC
-    # rules = {
-    #     "testbed":"slc",
-    #     "rules": 3,
-    #     "zones": ["COUNCIL DISTRICT 1","COUNCIL DISTRICT 2","COUNCIL DISTRICT 3"],
-    #     "strtypes": ["URML","URMM","URML"],
-    #     "pcts": [10,20,20]
-    # }
-    # retrofits = {
-    #     "ret_keys":["Wood or Metal Deck Diaphragms Retrofitted","Wood or Metal Deck Diaphragms Retrofitted","Wood or Metal Deck Diaphragms Retrofitted"],
-    #     "ret_vals":["","",""]        
-    # }
-
-    #### testing with 1 rule for SLC
-    # rules = {
-    #     "testbed":"slc",
-    #     "rules": 1,
-    #     "zones": ["COUNCIL DISTRICT 2"],
-    #     "strtypes": ["URML"],
-    #     "pcts": [10]
-    # }
-    # retrofits = {
-    #     "ret_keys":["Wood or Metal Deck Diaphragms Retrofitted"],
-    #     "ret_vals":[""]        
-    # }
+    return response.status_code
 
 
-    strategy_result_name = "retrofit_strategy_galveston"
-    cost_result_name = "rc_cost_slc"
-
-    con = get_connection(DATA_FILE)
-
-    config = {}
+def main():
+    rules_str = args.rules
+    rules = parse_rules(rules_str)
     if rules['testbed'] == "slc":
         config = SLC_CONFIG
     elif rules['testbed'] == "galveston":
@@ -180,6 +193,18 @@ if __name__ == "__main__":
     else:
         print("Invalid testbed")
         exit(1)
+
+    retrofits_str = args.retrofits
+    retrofits = parse_rules(retrofits_str)
+
+    # if no name is provided by the user, construct a name
+    strategy_result_name = args.result_name
+    if strategy_result_name is None:
+        strategy_result_name = f"Retrofit Strategy {rules['testbed']}"
+    cost_result_name = f"Retrofit Cost {rules['testbed']}"
+
+    # get the buildings from the database
+    con = get_connection(DATA_FILE)
 
     df_list = []
     for idx in range(rules['rules']):
@@ -196,22 +221,88 @@ if __name__ == "__main__":
         if df.shape[0] > 0:
             df_list.append(df)
 
-    merged_df = merge_create_retrofit_strategy(df_list, strategy_result_name)
+    # incore:retrofitStrategy
+    rs_df, rs_fname = merge_create_retrofit_strategy(df_list, strategy_result_name)
 
-    # # calculate retrofit cost
+    # calculate retrofit cost and other details
+    # incore:retrofitStrategyDetail
+    rs_detail_df = pd.DataFrame()
     if rules['testbed'] == "slc":
         ret_cost_df = rc_slc.get_retrofit_cost(con)
-        final_df = rc_slc.compute_retrofit_cost(cost_result_name, merged_df, ret_cost_df)    
+        rs_detail_df = rc_slc.compute_retrofit_cost(cost_result_name, rs_df, ret_cost_df)
     elif rules['testbed'] == "galveston":
         ret_cost_df = rc_galveston.get_retrofit_cost(con)
-        final_df = rc_galveston.compute_retrofit_cost(cost_result_name, merged_df, ret_cost_df, 1.79)  
+        rs_detail_df = rc_galveston.compute_retrofit_cost(cost_result_name, rs_df, ret_cost_df, 1.79)
     elif rules['testbed'] == "joplin":
         pass
     else:
         print("Invalid testbed")
         exit(1)
 
-    print(final_df.columns)
+    # return retrofit strategy details in dict format
+    rs_details_dict = rs_detail_df.to_dict('records')
+
     # create geospatial data of retrofit strategy (with cost)
-    create_geo_retrofit_strategy(final_df, strategy_result_name)
+    rs_details_geo_df, rs_details_geo_fname = create_geo_retrofit_strategy(rs_detail_df, strategy_result_name)
+
+    # close the db connection
     con.close()
+
+    # ----------------- Post retrofit strategy to the service -----------------
+    token = args.token
+    service_url = args.service_url
+    spaces = []
+    if args.spaces is not None and len(args.spaces) > 0:
+        spaces = args.spaces.strip().split(",")
+    source_id = args.source_id
+
+
+    # Create IN-CORE client
+    client = IncoreClient(service_url, token)
+
+    # Data Service
+    dataservice = DataService(client)
+    spaceservice = SpaceService(client)
+
+    # post retrofit strategy csv to the service
+    rs_dataset_id = store_results(dataservice,
+                                  spaceservice,
+                                  source_id=None,  # Don't join with parent dataset
+                                  title=strategy_result_name,
+                                  local_file=rs_fname,
+                                  data_type="incore:retrofitStrategy",
+                                  output_format="table",
+                                  spaces=spaces)
+
+    # post retrofit strategy detail shapefile to service
+    rs_detail_layer_id = store_results(dataservice,
+                                       spaceservice,
+                                       source_id=None,  # Don't join with parent dataset
+                                       title=f"{strategy_result_name} Details",
+                                       local_file=rs_details_geo_fname,
+                                       data_type="incore:retrofitStrategyDetail",
+                                       output_format="shapefile",
+                                       spaces=spaces)
+
+    # post retrofit strategy detail json to maestro
+    status = post_retrofit_summary(service_url, rs_dataset_id, rules, retrofits, rs_details_dict, rs_detail_layer_id)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Cluster building functionality results.')
+    parser.add_argument('--result_name', dest='result_name', help='Result Name')
+    parser.add_argument('--token', dest='token', help='Service token')
+    parser.add_argument('--service_url', dest='service_url', help='Service endpoint')
+    parser.add_argument('--buildings', dest='buildings', help='Building Inventory')
+    parser.add_argument('--functionality_probability', dest='functionality_probability',
+                        help='Building Functionality Probability')
+    parser.add_argument('--archetype_mapping', dest='archetype_mapping', help='Archetype Mapping')
+    parser.add_argument('--arch_col', dest='arch_col', help='Column to match on')
+    # parser = argparse.ArgumentParser(description='Generating Retrofit Strategy Dataset and Computing Retrofit Cost')
+    # parser.add_argument('--strategy_result_name', dest='strategy_result_name', help='Strategy Result name')
+    # parser.add_argument('--cost_result_name', dest='cost_result_name', help='Cost result name')
+    # parser.add_argument('--rules', dest='rules', type=json.loads, help='Retrofit strategy rules')
+    # parser.add_argument('--retrofits', dest='retrofits', type=json.loads, help='Retrofits')
+
+    args = parser.parse_args()
+    main()

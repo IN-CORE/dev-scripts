54e0b8148041f96734fdbe1774944c90c5f06de8
YONG WOOK KIM <ywkim312@users.noreply.github.com>
2025-03-26 14:10:12 -0500

    converting missing states

--- nsi/convert_nsi_to_building_inventory.py
+++ nsi/convert_nsi_to_building_inventory.py
@@ -6,29 +6,26 @@ from pyincore_data.nsibuildinginventory import NsiBuildingInventory
 from pyincore_data.utils.nsiutil import NsiUtil
 
 
-def create_building_inventory_by_geojson():
-    in_json = 'test1.json'
-    gdf = NsiBuildingInventory.convert_nsi_to_building_inventory_from_geojson(in_json, "westCoast")
-    gdf.to_file('test1.gpkg', driver='GPKG')
-
-
 if __name__ == '__main__':
     json_path = "nsi_data"
     # reade state group csv file
-    state_group = pd.read_csv("StateToGroup.csv")
+    state_group = pd.read_csv("StateToGroup_missing.csv")
 
     # create the combined state group dictionary
     STATE_REGION = dict(zip(state_group['Abbreviation'], state_group['Group']))
 
     # iterate with the STATE_REGION dictionary
     for state, region in STATE_REGION.items():
-        json_file = os.path.join(json_path, f"{state}.geojson")
+        # if it is geojson
+        # in_file = os.path.join(json_path, f"{state}.geojson")
+        # if it is geopackage
+        in_file = os.path.join(json_path, f"{state}.gpkg")
         # check if json file exists
-        if os.path.exists(json_file):
+        if os.path.exists(in_file):
             out_gpkg = os.path.join(json_path, f"{state}.gpkg")
             print(f"State: {state}, Region: {region}")
             # create building inventory by geojson
-            gdf = NsiBuildingInventory.convert_nsi_to_building_inventory_from_geojson(json_file, region)
+            gdf = NsiBuildingInventory.convert_nsi_to_building_inventory_from_geojson(in_file, region)
             gdf.to_file(out_gpkg, driver='GPKG')
         else:
             print(f"State: {state}, Region: {region} - No GeoJSON file found")
